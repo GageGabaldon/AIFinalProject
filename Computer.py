1197fc5409c4a3d7a15cb1eecfcdc7f6496225a6
Gage Gabaldon <ggmangabaldon@gmail.com>
2021-04-13 15:48:52 -0700

    yo stub out

--- Computer.py
+++ Computer.py
@@ -1,12 +1,21 @@
 class Computer:
 
-    def __init__(self, board, gui, whatSide):
+    def __init__(self, board, whatSide, myTurn):
         self.board = board
-        self.gui = gui
-        if(whatSide == "green"):
-            self.whatSide = "red"
-        else:
-            self.whatSide = "green"
+        self.whatSide = whatSide
+        self.turn = myTurn
+
+
+    def MakeAMove(self):
+        pass
+
+    # this prunes the sub trees of the search space.
+    def AlphaPrunning(self):
+        pass
+
+    # this just compares every piece to the goal using a distance formula
+    def winning(self):
+        pass
 
 
 
